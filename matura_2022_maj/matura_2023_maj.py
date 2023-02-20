2dd2fb84fe69e837562e6a7754b9c47385d93fca
rafal-nowak <contact@rafal-nowak.com>
2023-02-20 21:14:56 +0100

    refactor

--- matura_2022_maj/matura_2023_maj.py
+++ matura_2022_maj/matura_2023_maj.py
@@ -20,24 +20,26 @@ class Matura2023Maj:
     def __init__(self):
         pass
 
-    def zadanie41(selfs):
+    def zadanie_4_1(selfs):
         # Podaj, ile jest w pliku takich liczb, ktorych cyfry pierwsza i ostatnia sa takie same
         # Zapisz te z nich, ktora wystepuje w pliku jako pierwsza
+
+        number_of_searched_numbers = 0
+        first_number_as_text = ""
 
         with open("liczby.txt") as file:
 
             lines = [line.rstrip() for line in file]
-            number_of_searched_numbers = 0
-            first_number_as_text = ""
+
             for line in lines:
                 if is_first_and_last_digit_same(line):
                     number_of_searched_numbers += 1
                     if first_number_as_text == "":
                         first_number_as_text = line
 
-            return number_of_searched_numbers, first_number_as_text
+        return number_of_searched_numbers, first_number_as_text
 
-    def zadanie42(selfs):
+    def zadanie_4_2(selfs):
         # Znajdz w pliku liczbe, ktora ma w rozkladzie najwiecej czynnikow pierwszych
         # (podaj te liczbe oraz liczbe jej czynnikow pierwszych)
         # Znajdz w pliku liczbe, ktora ma w rozkladzie najwiecej roznych czynnikow pierwszych
@@ -62,22 +64,24 @@ class Matura2023Maj:
                     max_number_of_different_prime_factors = len(different_prime_factors)
                     number_with_max_different_prime_factors = number
 
-            return number_with_max_prime_factors, max_number_of_prime_factors, number_with_max_different_prime_factors, max_number_of_different_prime_factors
+        return number_with_max_prime_factors, max_number_of_prime_factors, number_with_max_different_prime_factors, max_number_of_different_prime_factors
 
-    def zadanie43(selfs):
+    def zadanie_4_3(selfs):
         # Podaj, ile jest dobrych trójek wśród liczb występujących w pliku `liczby.txt`.
         # Zapisz wszystkie dobre trójki do pliku `trojki.txt`
         # Podaj, ile jest dobrych piątek wśród liczb występujących w pliku `liczby.txt`.
 
+        # Dobre trojki
+        numer_of_good_threes = 0
+        good_threes = set()
+        # Dobre piatki
+        numer_of_good_fives = 0
+        good_fives = set()
+
         with open("liczby.txt") as file:
 
             lines = [line.rstrip() for line in file]
-            # Dobre trojki
-            numer_of_good_threes = 0
-            good_threes = set()
-            # Dobre piatki
-            numer_of_good_fives = 0
-            good_fives = set()
+
 
             numbers = set()
             for line in lines:
@@ -100,4 +104,4 @@ class Matura2023Maj:
                                                 numer_of_good_fives += 1
                                                 good_fives.add(f'{number} {i} {j} {k} {l}')
 
-            return numer_of_good_threes, good_threes, numer_of_good_fives, good_fives
+        return numer_of_good_threes, good_threes, numer_of_good_fives, good_fives
--- matura_2022_maj/test_matura_2023_maj.py
+++ matura_2022_maj/test_matura_2023_maj.py
@@ -4,16 +4,16 @@ from matura_2022_maj.matura_2023_maj import Matura2023Maj
 
 
 class TestMatura2023Maj(TestCase):
-    def test_zadanie41(self):
+    def test_zadanie_4_1(self):
         expected_number_of_searched_numbers = 18
         expected_first_number_as_text = "93639"
 
-        number_of_searched_numbers, first_number_as_text = Matura2023Maj().zadanie41()
+        number_of_searched_numbers, first_number_as_text = Matura2023Maj().zadanie_4_1()
 
         self.assertEqual(expected_number_of_searched_numbers, number_of_searched_numbers)
         self.assertEqual(expected_first_number_as_text, first_number_as_text)
 
-    def test_zadanie42(self):
+    def test_zadanie_4_2(self):
         expected_number_with_max_prime_factors = 99792
         expected_max_number_of_prime_factors = 10
         expected_number_with_max_different_prime_factors = 62790
@@ -22,14 +22,14 @@ class TestMatura2023Maj(TestCase):
         number_with_max_prime_factors, \
             max_number_of_prime_factors, \
             number_with_max_different_prime_factors, \
-            max_number_of_different_prime_factors = Matura2023Maj().zadanie42()
+            max_number_of_different_prime_factors = Matura2023Maj().zadanie_4_2()
 
         self.assertEqual(expected_number_with_max_prime_factors, number_with_max_prime_factors)
         self.assertEqual(expected_max_number_of_prime_factors, max_number_of_prime_factors)
         self.assertEqual(expected_number_with_max_different_prime_factors, number_with_max_different_prime_factors)
         self.assertEqual(expected_max_number_of_different_prime_factors, max_number_of_different_prime_factors)
 
-    def test_zadanie43(self):
+    def test_zadanie_4_3(self):
         expected_numer_of_good_threes = 27
         expected_good_threes = {'797 7173 64557', '409 9816 58896', '971 6797 27188', '871 15678 62712',
                                 '232 13688 27376', '2806 8418 84180', '955 8595 42975', '6797 13594 81564',
@@ -41,7 +41,7 @@ class TestMatura2023Maj(TestCase):
         expected_numer_of_good_fives = 2
         expected_good_fives = {'1403 2806 8418 42090 84180', '971 6797 13594 27188 81564'}
 
-        numer_of_good_threes, good_threes, numer_of_good_fives, good_fives = Matura2023Maj().zadanie43()
+        numer_of_good_threes, good_threes, numer_of_good_fives, good_fives = Matura2023Maj().zadanie_4_3()
 
         self.assertEqual(expected_numer_of_good_threes, numer_of_good_threes)
         self.assertEqual(expected_good_threes, good_threes)
