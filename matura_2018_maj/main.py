59139a1d370859d672b7b334085cb1f642addf77
wojciechjurewicz <62265579+wojciechjurewicz@users.noreply.github.com>
2023-03-20 17:42:28 +0100

    Update main.py

--- matura_2018_maj/main.py
+++ matura_2018_maj/main.py
@@ -1,82 +1,89 @@
-with open('sygnaly.txt', 'r') as file:
-    signals = file.read().split()
+if __name__ == '__main__':
+    with open('sygnaly.txt', 'r') as file:
+        signals = file.read().split()
 
 
-##  Zadanie 1 ##
+    ##  Zadanie 1 ##
 
-# Naukowcy zauważyli, że po złączeniu dziesiątych liter co czterdziestego słowa
-# (zaczynając od słowa czterdziestego) otrzymamy pewne przesłanie. Wypisz to przesłanie.
+    # Naukowcy zauważyli, że po złączeniu dziesiątych liter co czterdziestego słowa
+    # (zaczynając od słowa czterdziestego) otrzymamy pewne przesłanie. Wypisz to przesłanie.
 
-answer1_temp = []
-for i in range(39,len(signals),40):
-    answer1_temp.append(signals[i][9])
-answer1 = ''.join(answer1_temp)
+    message_temp = []
+    for i in range(39,len(signals),40):
+        message_temp.append(signals[i][9])
+    message = ''.join(message_temp)
 
-print("Zadanie 1\n")
-print("Odpowiedź do zadania:")
-print(answer1)
-print("\n\n")
+    print()
+    print('################################################################################')
+    print('Naukowcy zauważyli, że po złączeniu dziesiątych liter co czterdziestego słowa (zaczynając od słowa czterdziestego) otrzymamy pewne przesłanie. Wypisz to przesłanie.')
+    print(f'To przesłanie to {message}')
+    print('################################################################################')
+    print()
 
-## Zadanie 2 ##
+    ## Zadanie 2 ##
 
-# Znajdź słowo, w którym występuje największa liczba różnych liter.
-# Wypisz to słowo i liczbę występujących w nim różnych liter.
-# Jeśli słów o największej liczbie różnych liter jest więcej niż jedno,
-# wypisz pierwsze z nich pojawiające się w pliku z danymi
+    # Znajdź słowo, w którym występuje największa liczba różnych liter.
+    # Wypisz to słowo i liczbę występujących w nim różnych liter.
+    # Jeśli słów o największej liczbie różnych liter jest więcej niż jedno,
+    # wypisz pierwsze z nich pojawiające się w pliku z danymi
 
-def counting_different_letters(word):
-    letter_set = set()
-    for i in range(len(word)):
-        letter_set.add(word[i])
-    return len(letter_set)
+    def counting_different_letters(word):
+        letter_set = set()
+        for i in range(len(word)):
+            letter_set.add(word[i])
+        return len(letter_set)
 
-max_letter_number  = 0
-max_letter_word = ''
+    max_letter_number = 0
+    max_letter_word = ''
 
-for i in range(len(signals)):
-    if counting_different_letters(signals[i]) > max_letter_number:
-        max_letter_word = signals[i]
-        max_letter_number = counting_different_letters(signals[i])
+    for i in range(len(signals)):
+        if counting_different_letters(signals[i]) > max_letter_number:
+            max_letter_word = signals[i]
+            max_letter_number = counting_different_letters(signals[i])
 
-answer2 = "{0} {1}".format(max_letter_word, max_letter_number)
-
-print("Zadanie 2\n")
-print("Odpowiedź do zadania:")
-print(answer2)
-print("\n\n")
-
-
-## Zadanie 3 ##
-
-# W tym zadaniu rozważmy odległość liter w alfabecie –
-# np. litery A i B są od siebie oddalone o 1,A i E o 4,F i D o 2, a każda litera od siebie samej jest oddalona o 0.
-# Wypisz wszystkie słowa, w których każde dwie litery oddalone są od siebie w alfabecie co najwyżej o 10.
-# Słowa wypisz w kolejności występowania w pliku sygnaly.txt, po jednym w wierszu.
-
-def checking_if_distance_between_letters_greater_than_ten(word):
-    truth = 1
-    min_letter_ord = 1000
-    max_letter_ord = 0
-
-    for i in range(len(word)):
-        if ord(word[i]) > max_letter_ord:
-            max_letter_ord = ord(word[i])
-        if ord(word[i]) < min_letter_ord:
-            min_letter_ord = ord(word[i])
-    if max_letter_ord - min_letter_ord > 10:
-        truth = 0
-    return truth
+    print()
+    print('################################################################################')
+    print('Znajdź słowo, w którym występuje największa liczba różnych liter.')
+    print('Wypisz to słowo i liczbę występujących w nim różnych liter.')
+    print('Jeśli słów o największej liczbie różnych liter jest więcej niż jedno, wypisz pierwsze z nich pojawiające się w pliku z danymi')
+    print(f'To słowo to {max_letter_word}. Występuje w nim {max_letter_number} różnych liter')
+    print('################################################################################')
+    print()
 
 
-answer3_temp = []
+    ## Zadanie 3 ##
 
-for i in range(len(signals)):
-    if checking_if_distance_between_letters_greater_than_ten(signals[i]) == 1:
-        answer3_temp.append(signals[i])
+    # W tym zadaniu rozważmy odległość liter w alfabecie –
+    # np. litery A i B są od siebie oddalone o 1,A i E o 4,F i D o 2, a każda litera od siebie samej jest oddalona o 0.
+    # Wypisz wszystkie słowa, w których każde dwie litery oddalone są od siebie w alfabecie co najwyżej o 10.
+    # Słowa wypisz w kolejności występowania w pliku sygnaly.txt, po jednym w wierszu.
 
-answer3 = '\n'.join(answer3_temp)
-print("Zadanie 3\n")
-print("Odpowiedź do zadania:")
-print(answer3)
-print('\n\n')
-print(answer3_temp)
+    def checking_if_distance_between_letters_greater_than_ten(word):
+        min_letter_ord = 1000
+        max_letter_ord = 0
+
+        for i in range(len(word)):
+            if ord(word[i]) > max_letter_ord:
+                max_letter_ord = ord(word[i])
+            if ord(word[i]) < min_letter_ord:
+                min_letter_ord = ord(word[i])
+        if max_letter_ord - min_letter_ord > 10:
+            return False
+        return True
+
+
+    words = []
+
+    for i in range(len(signals)):
+        if checking_if_distance_between_letters_greater_than_ten(signals[i]) == True:
+            words.append(signals[i])
+
+    words = '\n'.join(words)
+
+    print()
+    print('################################################################################')
+    print('Wypisz wszystkie słowa, w których każde dwie litery oddalone są od siebie w alfabecie co najwyżej o 10.')
+    print('Słowa wypisz w kolejności występowania w pliku sygnaly.txt, po jednym w wierszu.')
+    print(f'Te słowa to {words}')
+    print('################################################################################')
+    print()
