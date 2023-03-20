6984e10ab1af888ccbd8f854c7b9f9dbe81b4f7b
BarWdo <62766173+BarWdo@users.noreply.github.com>
2023-03-20 22:47:27 +0100

    Add files via upload

--- matura_kwiecien_2020/main.py
+++ matura_kwiecien_2020/main.py
@@ -1,13 +1,13 @@
 # Podaj wartość największej luki oraz wartość najmniejszej luki pomiędzy elementami ciągu
 print("1.1")
 with open("dane4.txt", "r") as f:
-    ciag = [int(x) for x in f.read().split()]
+    string = [int(x) for x in f.read().split()]
 
 min_gap = float('inf')
 max_gap = 0
 
-for i in range(1, len(ciag)):
-    gap = abs(ciag[i] - ciag[i-1])
+for i in range(1, len(string)):
+    gap = abs(string[i] - string[i-1])
     if gap < min_gap:
         min_gap = gap
     if gap > max_gap:
@@ -20,24 +20,24 @@ print("Największa luka:", max_gap)
 # Podaj jego długość oraz wartości (liczby) znajdujące się na początku i końcu tego fragmentu.
 print("1.2")
 with open("dane4.txt") as f:
-    ciag = [int(x) for x in f.read().split()]
+    string = [int(x) for x in f.read().split()]
 
-n = len(ciag)
+n = len(string)
 
-def find_regular(ciag):
+def find_regular(string):
     result = []
     for i in range(n-1):
-        regular = [ciag[i], ciag[i+1]]
-        gap = abs(ciag[i+1] - ciag[i])
+        regular = [string[i], string[i+1]]
+        gap = abs(string[i+1] - string[i])
         for j in range(i+1, n-1):
-            if abs(ciag[j+1] - ciag[j]) != gap:
+            if abs(string[j+1] - string[j]) != gap:
                 break
-            regular.append(ciag[j+1])
+            regular.append(string[j+1])
         if len(regular) > len(result):
             result = regular
     return result
 
-regular = find_regular(ciag)
+regular = find_regular(string)
 print("Największa długość fragmentu regularnego:", len(regular))
 print("Początek:", regular[0])
 print("Koniec:", regular[-1])
--- matura_kwiecien_2020/matura_kwiecien_2020.py
+++ matura_kwiecien_2020/matura_kwiecien_2020.py
@@ -1,18 +1,18 @@
 
-class Matura_Kwiecień_2020:
+class matura_kwiecien_2020:
     def __init__(self):
         pass
 # Podaj wartość największej luki oraz wartość najmniejszej luki pomiędzy elementami ciągu
 
     def zadanie_4_1(selfs):
         with open("dane4.txt", "r") as f:
-            ciag = [int(x) for x in f.read().split()]
+            string = [int(x) for x in f.read().split()]
 
         min_gap = float('inf')
         max_gap = 0
 
-        for i in range(1, len(ciag)):
-            gap = abs(ciag[i] - ciag[i-1])
+        for i in range(1, len(string)):
+            gap = abs(string[i] - string[i-1])
             if gap < min_gap:
                 min_gap = gap
             if gap > max_gap:
@@ -26,25 +26,22 @@ class Matura_Kwiecień_2020:
 
     def zadanie_4_2(selfs):
         with open("dane4.txt") as f:
-            ciag = [int(x) for x in f.read().split()]
+            string = [int(x) for x in f.read().split()]
 
-        n = len(ciag)
+        n = len(string)
 
-        def find_regular(ciag):
-            result = []
-            for i in range(n-1):
-                regular = [ciag[i], ciag[i+1]]
-                gap = abs(ciag[i+1] - ciag[i])
-                for j in range(i+1, n-1):
-                    if abs(ciag[j+1] - ciag[j]) != gap:
-                        break
-                    regular.append(ciag[j+1])
-                if len(regular) > len(result):
-                    result = regular
-            return result
+        result = []
+        for i in range(n-1):
+            regular = [string[i], string[i+1]]
+            gap = abs(string[i+1] - string[i])
+            for j in range(i+1, n-1):
+                if abs(string[j+1] - string[j]) != gap:
+                    break
+                regular.append(string[j+1])
+            if len(regular) > len(result):
+                result = regular
 
-        regular = find_regular(ciag)
-        return len(regular), regular[0], regular[-1]
+        return len(result), result[0], result[-1]
 
     # Podaj krotność najczęstszej luki oraz wartości wszystkich najczęstszych luk w ciągu
 
@@ -52,20 +49,19 @@ class Matura_Kwiecień_2020:
         with open("dane4.txt", "r") as f:
             numbers = [int(x) for x in f.read().split()]
 
-        def find_frequent_gaps(numbers):
-            gaps = []
-            for i in range(1, len(numbers)):
-                gaps.append(abs(numbers[i] - numbers[i-1]))
-            gaps_count = {}
-            for gap in gaps:
-                if gap in gaps_count:
-                    gaps_count[gap] += 1
-                else:
-                    gaps_count[gap] = 1
-            max_count = max(gaps_count.values())
-            most_frequent_gaps = [gap for gap, count in gaps_count.items() if count == max_count]
-            return max_count, most_frequent_gaps
+        gaps = []
+        for i in range(1, len(numbers)):
+            gaps.append(abs(numbers[i] - numbers[i-1]))
+        gaps_count = {}
+        for gap in gaps:
+            if gap in gaps_count:
+                gaps_count[gap] += 1
+            else:
+                gaps_count[gap] = 1
+        max_count = max(gaps_count.values())
+        most_frequent_gaps = [gap for gap, count in gaps_count.items() if count == max_count]
+        return max_count, most_frequent_gaps
 
-        return find_frequent_gaps(numbers)
+
 
 
