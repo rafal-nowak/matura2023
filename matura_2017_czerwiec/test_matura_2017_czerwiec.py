f13f3d6af53ebf0f065adc4133b4644c659f15eb
wojciechjurewicz <62265579+wojciechjurewicz@users.noreply.github.com>
2023-03-26 20:19:07 +0200

    Update test_matura_2017_czerwiec.py

--- matura_2017_czerwiec/test_matura_2017_czerwiec.py
+++ matura_2017_czerwiec/test_matura_2017_czerwiec.py
@@ -1,5 +1,5 @@
 import unittest
-from Matura_2017_czerwiec import Matura2017Czerwiec
+from matura_2017_czerwiec.matura_2017_czerwiec import Matura2017Czerwiec
 
 class TestMatura2015Maj(unittest.TestCase):
     def test_zadanie_4_1(self):
@@ -7,7 +7,7 @@ class TestMatura2015Maj(unittest.TestCase):
 
         PrimeNum= Matura2017Czerwiec().zadanie_4_1()
 
-        self.assertEqual( expected_PrimeNum, PrimeNum)
+        self.assertEqual(expected_PrimeNum, PrimeNum)
 
     def test_zadanie_4_2(self):
         expected_n = 5
@@ -25,22 +25,22 @@ class TestMatura2015Maj(unittest.TestCase):
 
         lenght, XA, YA, XB, YB = Matura2017Czerwiec().zadanie_4_3()
 
-        self.assertEqual(  expected_lenght, lenght)
-        self.assertEqual(  expected_xa, XA)
-        self.assertEqual(  expected_ya, YA)
-        self.assertEqual(  expected_xb, XB)
-        self.assertEqual(  expected_yb, YB)
+        self.assertEqual(expected_lenght, lenght)
+        self.assertEqual(expected_xa, XA)
+        self.assertEqual(expected_ya, YA)
+        self.assertEqual(expected_xb, XB)
+        self.assertEqual(expected_yb, YB)
 
     def test_zadanie_4_4(self):
         expected_outside = 749
         expected_inside = 249
         expected_edge = 2
 
-        outside,inside, edge = Matura2017Czerwiec().zadanie_4_4()
+        inside,outside, edge = Matura2017Czerwiec().zadanie_4_4()
 
-        self.assertEqual(  expected_outside, outside)
-        self.assertEqual(  expected_inside, inside)
-        self.assertEqual(  expected_edge, edge)
+        self.assertEqual(expected_outside, outside)
+        self.assertEqual(expected_inside, inside)
+        self.assertEqual(expected_edge, edge)
 
 if __name__ == '__main__':
     unittest.main()
